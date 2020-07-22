11c701f43d381ad115b1afec4a00d17aafc841fe
enchance <enchance@gmail.com>
2020-07-22 18:54:13 +0800

    Static files: SCSS + JS + base.tpl

---
+++ {{cookiecutter.project_slug}}/staticdev/js/init.js
@@ -0,0 +1,24 @@
+/**
+ * Check the env according to bootstrap
+ * @returns {string}
+ */
+function checkEnv() {
+    let envs = ['xs', 'sm', 'md', 'lg', 'xl'];
+
+    let el = document.createElement('div');
+    document.body.appendChild(el);
+
+    let curEnv = envs.shift();
+
+    for (let env of envs.reverse()) {
+        el.classList.add(`d-${env}-none`);
+
+        if (window.getComputedStyle(el).display === 'none') {
+            curEnv = env;
+            break;
+        }
+    }
+
+    document.body.removeChild(el);
+    return curEnv;
+}
