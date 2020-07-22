384a2d221c6ddd6534d30bc0d5d79329d305a553
enchance <enchance@gmail.com>
2020-07-22 18:21:17 +0800

    Add project-level settings file

---
+++ {{cookiecutter.project_slug}}/config/settings/settings.py
@@ -0,0 +1,15 @@
+# Populated via signal
+CACHE_SELECTION_BY_ID = 'selection:idset'
+CACHE_SELECTION_BY_PARENT = 'selection:parentset'
+TTL_DAY = 3600 * 24
+TTL_SHORT = TTL_DAY * 2
+TTL_MONTH = TTL_DAY * 31
+TTL_FORM = 500
+
+# Main cached data
+CACHE_OPTIONS = 'user-%s:options'           # user.id
+CACHE_FORM = 'user-%s:form'                 # user.id
+CACHE_NAME_REFERENCE = {
+    # Forms here
+    # UI here
+}
